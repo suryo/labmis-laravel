f7ef0c9b313c0307d41e1ce8eeda4ab230e2f0d3
suryo <suryoatm@gmail.com>
2023-01-10 17:10:42 +0700

    Mata kuliah

---
+++ app/Models/MatakuliahModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MatakuliahModel extends Model
+{
+    use HasFactory;
+}
